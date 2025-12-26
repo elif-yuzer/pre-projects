ebc3e28302f72d497f81a31a33ab946f77f5edc9
Elif Yuzer <elifyuzer.12@gmail.com>
2025-12-26 03:56:28 +0100

    feat: Complete Pokedex app with dynamic theming and error handling
    - Implement fetch API to retrieve data from PokeAPI
    - Add async/await function with try-catch block for error management
    - Create responsive UI using CSS Grid and Layers
    - Enable 'Enter' key support for better user experience
    - Implement dynamic CSS classes for Pokemon type-based background colors

---
+++ digital-pokedox/app.js
@@ -0,0 +1,52 @@
+const girilenInput = document.getElementById("poke-input");
+const searchBtn = document.querySelector("#search-btn");
+const pokeResult = document.getElementById("poke-result");
+const PokemonCard=document.querySelector(".pokemon-card")
+
+const veriCek = async (isim) => {
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${isim.toLowerCase()}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    const resimSrc = data.sprites.other.dream_world.front_default || data.sprites.front_default;
+    const tur = data.types[0].type.name;
+    pokeResult.innerHTML = `
+        <div class="pokemon-card theme-${tur}">
+            <img class="poke-img" src="${resimSrc}" alt="${data.name}" />
+            <div class="poke-info">
+                <h3>${data.name}</h3>
+                <span class="badge">${tur}</span>
+            </div>
+        </div>
+    `;
+
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+    pokeResult.innerHTML = "";
+  }
+};
+
+searchBtn.addEventListener("click", () => {
+  const girilenDeger = girilenInput.value;
+  console.log(girilenDeger);
+  if (girilenDeger !== "") {
+    veriCek(girilenDeger);
+  } else {
+    alert("Lütfen birisim girin");
+  }
+});
+
+girilenInput.addEventListener("keydown",(e)=>{
+    if(e.key=="Enter" ){
+        searchBtn.click()
+    }
+
+})
+
